fa0edd07b05df21df446b1942827d00e523d1636
feps <120133472+fepskieee@users.noreply.github.com>
2024-01-05 07:12:47 +0800

    feat: Added username for each users in chat message.

--- socketAPI.js
+++ socketAPI.js
@@ -1,15 +1,27 @@
 import { Server } from "socket.io";
+import { generateUsername } from "unique-username-generator";
 
 const io = new Server();
 const socketAPI = { io: io };
 
-io.on( "connection", function(socket) {
+io.on("connection", function(socket) {
   console.log( "A user connected" );
+  let users = {}
+
+  const id = socket.id.substring(0, 2);
+  const username = generateUsername("", 0, 5);
+
+  if (!users.hasOwnProperty(id)) {
+    users[id]
+  }
+
+  users[id] = username
 
   socket.on('message', (message) =>     {
-    console.log(message);
+    console.log(`${users[id]}: ${message}`);
+
     io.emit('message', {
-      id: socket.id.substring(0,2),
+      id: users[id],
       message
     });
   });
